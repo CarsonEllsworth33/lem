71b18d20eee0d2d53dd0567d0a116e0ed9214901
carson ellsworth <carson.a.ellsworth.civ@us.navy.mil>
2025-04-22 15:55:41 -0400

    added other attributes

--- contrib/strata/strata.lisp
+++ contrib/strata/strata.lisp
@@ -5,19 +5,23 @@
 
 (define-major-mode strata-mode nil
     (:name "strata"
-     :keymap *strata-mode-keymap*))
+     :keymap *strata-mode-keymap*)) 
 
 ;; Define constants here
 
 (defparameter *faction-attributes* 
   (map 'vector (lambda (color-name) (make-attribute :foreground color-name)) 
-       '("red" "cyan" "yellow" "green")))
+       '("red" "cyan" "yellow" "green"))) 
 
-(defvar *strata-buffer*)
+(defparameter *wood-attribute* (make-attribute :foreground "brown"))
+(defparameter *food-attribute* (make-attribute :foreground "magenta"))
+(defparameter *wall-attribute* (make-attribute :foreground "gray"))
+
+(defvar *strata-buffer*) 
 
 (define-command strata () ()
-  (setf *strata-buffer* (make-buffer "*strata*"))
+  (setf *strata-buffer* (make-buffer "*strata*")) 
   (switch-to-buffer *strata-buffer*)
   (strata-mode)
   (add-hook (variable-value 'kill-buffer-hook :buffer *strata-buffer*)
-            #'(lambda (buffer) (declare (ignore buffer)) (strata-quit))))
+            #'(lambda (buffer) (declare (ignore buffer)) (strata-quit)))) 
