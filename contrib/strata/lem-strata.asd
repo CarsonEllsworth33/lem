d1aa70940cb9296de8fbf14db53898dd4932e8f5
dunderscore <carsone2017@gmail.com>
2025-04-22 21:30:52 -0400

    fleshing out strata more to learn about lem internals

---
+++ contrib/strata/lem-strata.asd
@@ -0,0 +1,4 @@
+(defsystem "lem-strata"
+  :serial t
+  :depends-on ("lem")
+  :components ((:file "strata")))
--- contrib/strata/strata.lisp
+++ contrib/strata/strata.lisp
@@ -3,25 +3,82 @@
 
 (in-package :lem.strata) 
 
-(define-major-mode strata-mode nil
-    (:name "strata"
-     :keymap *strata-mode-keymap*)) 
 
 ;; Define constants here
+(defvar *playing-p*) 
+(defvar *strata-buffer*) 
+(defvar *player*)
+
+(defconstant +world-width+ 80) 
+(defconstant +world-height+ 80) 
+
 
 (defparameter *faction-attributes* 
   (map 'vector (lambda (color-name) (make-attribute :foreground color-name)) 
        '("red" "cyan" "yellow" "green"))) 
 
-(defparameter *wood-attribute* (make-attribute :foreground "brown"))
-(defparameter *food-attribute* (make-attribute :foreground "magenta"))
-(defparameter *wall-attribute* (make-attribute :foreground "gray"))
+(defparameter *wood-attribute* (make-attribute :foreground "brown")) 
+(defparameter *food-attribute* (make-attribute :foreground "magenta")) 
+(defparameter *wall-attribute* (make-attribute :foreground "gray")) 
 
-(defvar *strata-buffer*) 
+(defun insert-food ()
+  (insert-string ))
+
+(defun random-world-point ()
+  (let ((x (random +world-width+))
+        (y (random +world-height+)))
+    (list y x))) 
+
+(defun strata-move-up (strata)
+  (incf (strata-get-y-pos strata)))
+(defun strata-move-down (strata)
+  (decf (strata-get-y-pos strata)))
+(defun strata-move-right (strata)
+  (incf (strata-get-x-pos strata)))
+(defun strata-move-left (strata)
+  (decf (strata-get-x-pos strata)))
+
+(defclass strata ()
+  ((world-position
+    :accessor world-position ;; (Y X) or (line, column)
+    :initform (random-world-point))
+   (health
+    :accessor health
+    :initarg :health
+    :initform 0)))
+
+(defun strata-get-y-pos (strata)
+  (car (world-position strata)))
+
+(defun strata-get-x-pos (strata)
+  (cdr (world-position strata)))
+
+
+
+;; Lem specific things
+(define-major-mode strata-mode nil
+    (:name "strata"
+     :keymap *strata-mode-keymap*)) 
 
 (define-command strata () ()
+  (setf *player* (make-instance 'strata))
   (setf *strata-buffer* (make-buffer "*strata*")) 
   (switch-to-buffer *strata-buffer*)
   (strata-mode)
   (add-hook (variable-value 'kill-buffer-hook :buffer *strata-buffer*)
             #'(lambda (buffer) (declare (ignore buffer)) (strata-quit)))) 
+
+(define-command move-player-up () ()
+  (strata-move-up *player*))
+(define-command move-player-down () ()
+  (strata-move-down *player*))
+(define-command move-player-left () ()
+  (strata-move-left *player*))
+(define-command move-player-right () ()
+  (strata-move-right *player*))
+
+;; Player Actions
+(define-key *strata-mode-keymap* "w" 'move-player-up)
+(define-key *strata-mode-keymap* "a" 'move-player-left)
+(define-key *strata-mode-keymap* "s" 'move-player-down)
+(define-key *strata-mode-keymap* "d" 'move-player-right)
